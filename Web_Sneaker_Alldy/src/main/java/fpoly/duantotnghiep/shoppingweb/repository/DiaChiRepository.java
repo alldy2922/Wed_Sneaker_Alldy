eac474d71b53808626d26d5c9cc2ffed84c9f1b4
datnvph26760 <118900203+datnvph26760@users.noreply.github.com>
2024-05-30 19:13:48 +0700

    delete dia chi

--- Web_Sneaker_Alldy/src/main/java/fpoly/duantotnghiep/shoppingweb/repository/DiaChiRepository.java
+++
@@ -1,8 +0,0 @@
-package fpoly.duantotnghiep.shoppingweb.repository;
-
-import fpoly.duantotnghiep.shoppingweb.model.DiaChiModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaChiRepository extends JpaRepository<DiaChiModel, Long> {
-
-}
--- Web_Sneaker_Alldy/src/main/java/fpoly/duantotnghiep/shoppingweb/service/IDiaChiServe.java
+++
@@ -1,9 +0,0 @@
-package fpoly.duantotnghiep.shoppingweb.service;
-
-import fpoly.duantotnghiep.shoppingweb.dto.DiaChiDtoResponse;
-
-import java.util.List;
-
-public interface IDiaChiServe {
-    List<DiaChiDtoResponse> findAll();
-}

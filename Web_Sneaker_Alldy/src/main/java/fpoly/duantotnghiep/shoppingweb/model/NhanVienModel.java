be164c1de76ce012043c9088eb5426ee6bf8c218
Ky Anh 03 <anhhkph21563@fpt.edu.vn>
2024-06-01 18:52:04 +0700

    login role admin

---
+++ Web_Sneaker_Alldy/src/main/java/fpoly/duantotnghiep/shoppingweb/model/NhanVienModel.java
@@ -0,0 +1,45 @@
+package fpoly.duantotnghiep.shoppingweb.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "nhanvien")
+public class NhanVienModel {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "vaitro")
+    private VaiTroModel vaiTro;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "hovaten")
+    private String hoVaTen;
+
+    @Column(name = "gioitinh")
+    private Boolean gioiTinh;
+
+    @Column(name = "ngaysinh")
+    private LocalDate ngaySinh;
+
+    @Column(name = "sodienthoai")
+    private String soDienThoai;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "anhdaidien")
+    private String anhDaiDien;
+
+}
---
+++ Web_Sneaker_Alldy/src/main/java/fpoly/duantotnghiep/shoppingweb/service/INhanVienService.java
@@ -0,0 +1,27 @@
+package fpoly.duantotnghiep.shoppingweb.service;
+
+import fpoly.duantotnghiep.shoppingweb.dto.reponse.NhanVienDtoResponse;
+import fpoly.duantotnghiep.shoppingweb.dto.request.NhanVienDtoRequest;
+import jakarta.mail.MessagingException;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface INhanVienService {
+
+    Page<NhanVienDtoResponse> getAll(Integer page, Integer limit);
+
+    NhanVienDtoResponse findById(String username);
+
+    Boolean existsByUsername(String username);
+
+
+    NhanVienDtoResponse add(NhanVienDtoRequest nhanVien) throws MessagingException, MessagingException;
+
+    NhanVienDtoResponse update(NhanVienDtoRequest nhanVien);
+
+    NhanVienDtoResponse update(NhanVienDtoRequest nhanVien, MultipartFile img) throws IOException;
+
+    void deleteByUsername(String username);
+}
